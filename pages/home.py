d4d712c90100647b5c06e4eafd6e50aa6f871ac7
Sergei Mikurov <454004@mail.ru>
2024-07-09 00:14:34 +0500

    Set title as instance attr

--- pages/home.py
+++ pages/home.py
@@ -6,7 +6,6 @@ from pages.base_page import TestingPage
 class HomePage(TestingPage):
     """The home page representation class."""
 
-    title = 'Домашняя страница'
-
     def __init__(self, page: Page) -> None:
         super().__init__(page)
+        self.title = 'Домашняя страница'
