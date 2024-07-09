e7b166b0d1d1516b820ae5262f56a22a6e9ae22e
Sergei Mikurov <454004@mail.ru>
2024-07-09 15:39:35 +0500

    Add create word page

---
+++ pages/word_pages.py
@@ -0,0 +1,21 @@
+from playwright.sync_api import Page
+
+from pages.base_page import TestingPage
+
+
+class CreateWordPage(TestingPage):
+    """Add word to english dictionary class."""
+
+    def __init__(self, page: Page) -> None:
+        super().__init__(page)
+        self.path = '/english/word/create'
+        self.title = 'Добавить слово в словарь'
+        self.english_word_input = page.get_by_label('Слово на английском')
+        self.russian_word_input = page.get_by_label('Слово на русском')
+        self.submit_button = page.get_by_role('button', name='Сохранить')
+
+    def create_word(self):
+        """Test add word to english dictionary."""
+        self.english_word_input.fill('карандаш')
+        self.english_word_input.fill('a pencil')
+        self.submit_button.click()
